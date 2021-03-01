19e847fe35f178e30580276078691b374fc8def7
NicolasCLEMENT1 <nicolas.clement@insa-rouen.fr>
2021-03-01 23:52:37 +0100

    Try another thing

---
+++ build/precache-manifest.dfd16f28b9a1c1757000d9d6040f4e15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003aa446c4d0e36e76e1ae8ed3849bfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6289a750b6cdee0f7128",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "8fe73d8dac3ae16ff9b5",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6289a750b6cdee0f7128",
+    "url": "/static/js/2.0f1b97c4.chunk.js"
+  },
+  {
+    "revision": "8fe73d8dac3ae16ff9b5",
+    "url": "/static/js/main.77c9cdf7.chunk.js"
+  },
+  {
+    "revision": "be4aa8fe25465bf7c843",
+    "url": "/static/js/runtime-main.c6d24bb2.js"
+  }
+]);
